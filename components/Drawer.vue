e1c8935f6d8518f658f9aff71234ea336b53908d
nickgayo12 <nickgayo12@gmail.com>
2020-12-09 15:18:59 +0800

    chore: componentize

---
+++ components/Drawer.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-navigation-drawer clipped clipped-left mini-variant fixed app>
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in drawerList"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-item-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawerList: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app-bar clipped-left fixed app class="header-box-shadow white">
+    logo
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
